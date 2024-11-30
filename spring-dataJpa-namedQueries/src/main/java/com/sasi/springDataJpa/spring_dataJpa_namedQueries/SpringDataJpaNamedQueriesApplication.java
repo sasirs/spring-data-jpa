f638c6ae3ac986e167c0da57882e37658999f1bc
Administrator <sasikumar.r@tejosma.com>
2024-11-30 13:38:37 +0530

    Project created

---
+++ spring-dataJpa-namedQueries/src/main/java/com/sasi/springDataJpa/spring_dataJpa_namedQueries/SpringDataJpaNamedQueriesApplication.java
@@ -0,0 +1,13 @@
+package com.sasi.springDataJpa.spring_dataJpa_namedQueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaNamedQueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaNamedQueriesApplication.class, args);
+	}
+
+}
---
+++ spring-dataJpa-namedQueries/src/test/java/com/sasi/springDataJpa/spring_dataJpa_namedQueries/SpringDataJpaNamedQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sasi.springDataJpa.spring_dataJpa_namedQueries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaNamedQueriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
